a8e0db8b53aab9687754e8608ac51f2365723739
chester <chester@spe3d.co>
2025-05-26 16:25:41 +0800

    fix: cert

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,21 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 import fs from "fs";
 
+// 判斷是否為開發模式
+const isDev = process.env.NODE_ENV !== "production";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, "cert/server.key")),
-      cert: fs.readFileSync(path.resolve(__dirname, "cert/server.crt")),
-    },
-    host: "0.0.0.0",
-  },
+  server: isDev
+    ? {
+        https: {
+          key: fs.readFileSync(path.resolve(__dirname, "cert/server.key")),
+          cert: fs.readFileSync(path.resolve(__dirname, "cert/server.crt")),
+        },
+        host: "0.0.0.0",
+      }
+    : undefined,
   resolve: {
     alias: {
       "@": "/src",
