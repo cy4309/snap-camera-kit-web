bf914aa71a0258aa4b7f46e6f49c8ddb44e6524c
chester <chester@spe3d.co>
2025-08-27 11:40:30 +0800

    update: snap camera kit npm

--- src/contexts/CameraKitContext.tsx
+++ src/contexts/CameraKitContext.tsx
@@ -9,10 +9,10 @@ const apiToken =
 //   "eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzQ3Mjk2MDkwLCJzdWIiOiIwNmYzNDAxNS1hMjhmLTQyODUtYWQyOC0wZmFmYjFiMjg5M2N-UFJPRFVDVElPTn5kZDlmY2ZiOS0xNDliLTRmYTEtYmMyOS0yODc5ZjQ3YWUwY2YifQ.4DCJ8eldekKmL5nFU3taVMTD891o9kU_sHojrPZ6pPc"; //jimi-prod
 
 // const lensGroupId = "39eb0d0a-9374-411a-95e1-63c3b44e2729"; //chester
-const lensGroupId = "9ad5f7fe-d069-4103-837e-4fee28bdd5af"; //jimi
+// const lensGroupId = "9ad5f7fe-d069-4103-837e-4fee28bdd5af"; //jimi
+const lensGroupId = "3fb26319-96cd-41e0-ae70-8a3b9fdf3723"; //jimi
 // const lensId_spe3d = "2f81fd00-196d-49f0-8da4-c540e6367070"; //chester
 // const lensId_segmentat = "43300180875"; //chester
-
 export interface CameraKitState {
   session: CameraKitSession;
   lenses: Lens[];
